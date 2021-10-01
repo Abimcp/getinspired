7e515311ebd1e845aad98c56e99a22925ce5489b
“cerise-at <cerise.athompson>
2021-10-01 11:53:50 +0100

    added completed quote model

--- backend/generator/models.py
+++ backend/generator/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class CompletedQuote(models.Model):
+    id = models.CharField(max_length=100, primary_key=True)
+    tweet = models.CharField(max_length=240)
+    font = models.CharField(max_length=100)
+    picture_url = models.CharField(max_length=2048)
